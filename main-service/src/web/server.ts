62e64519545a755ed1b1c77895d35646fa339005
aniket <aniketavinashmore343@gmail.com>
2021-09-11 19:49:36 +0530

    config dotenv

--- main-service/src/web/server.ts
+++ main-service/src/web/server.ts
@@ -1,9 +1,11 @@
 import { App } from './app';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const PORT = 8080;
 
 const app = new App([]);
 
-app.expressApp.listen(PORT,()=>{
+app.expressApp.listen(PORT, () => {
   console.log(`server started on port: ${PORT}`);
-})
+});
