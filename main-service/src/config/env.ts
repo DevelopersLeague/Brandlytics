ad23009b2df3fa0c9c093622efe72b82fc0bb227
aniket <aniketavinashmore343@gmail.com>
2021-09-20 00:04:40 +0530

    fix no file error bug

--- main-service/src/config/env.ts
+++ main-service/src/config/env.ts
@@ -4,13 +4,14 @@ import fs from 'fs';
 export function loadEnv(filename: string): void {
   const rootPath = path.join(__dirname, '..', '..');
   const filePath = path.join(rootPath, filename);
+  // returns if file is not found
   if (!fs.existsSync(filePath)) {
-    throw new Error(`file ${filePath} does not exists`);
+    return;
   }
   const fileContent = fs.readFileSync(filePath).toString();
   const fileJson = JSON.parse(fileContent);
   Object.keys(fileJson).forEach((key) => {
-    if (typeof fileJson[key] != "string") {
+    if (typeof fileJson[key] != 'string') {
       throw new Error('value for each key must be a string in env json files');
     }
     process.env[key] = fileJson[key];
