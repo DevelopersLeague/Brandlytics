0bf7547e5bfa2acedcc94fb5251209f8573d6520
aniket <aniketavinashmore343@gmail.com>
2021-09-22 12:32:46 +0530

    setup migration scripts

---
+++ main-service/src/scripts/migrate.ts
@@ -0,0 +1,19 @@
+import { loadEnv } from '../config/env';
+loadEnv('.env.json');
+import { knexInstance } from '../config/knex';
+
+async function main(): Promise<void> {
+  if (process.argv[2] === 'up') {
+    await knexInstance.migrate.latest();
+  } else if (process.argv[2] === 'down') {
+    await knexInstance.migrate.down();
+  } else {
+    throw new Error('invalid arguments');
+  }
+}
+main()
+  .then(() => {
+    console.log('Migration Complete');
+    process.exit();
+  })
+  .catch((err) => console.log(err));
