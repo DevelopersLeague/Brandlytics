4368bdd82dfda0f4ca7b17e836d151678381a584
aniket <aniketavinashmore343@gmail.com>
2021-09-24 00:09:16 +0530

    add api client library

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { APIClient } from "./lib/api";
+
+(async () => {
+  const client = new APIClient({
+    tokenKey: "token",
+    serverBaseUrl: process.env.REACT_APP_API_SERVER,
+  });
+  const user = await client.login({
+    username: "aniket_more3",
+    password: "password123",
+  });
+  console.log(user);
+})().catch(console.log);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ react-client/src/lib/api/index.js
@@ -0,0 +1,87 @@
+import jwt from "jsonwebtoken";
+/**
+ * @typedef {Object} User
+ * @property {number} sub
+ * @property {string} username
+ * @property {string} firstname
+ * @property {string} lastname
+ */
+
+export class APIClient {
+  /**
+   * @typedef {Object} APIClientOptions
+   * @property {string} tokenKey
+   * @property {string} serverBaseUrl
+   */
+  /**
+   *
+   * @param {APIClientOptions} options - options for initialization
+   */
+  constructor(options) {
+    this._tokenKey = options.tokenKey;
+    this._serverBaseUrl = options.serverBaseUrl;
+    /**@type {string} */
+    this._token = null;
+
+    const presetToken = localStorage.getItem(this._tokenKey);
+    if (presetToken) {
+      this._token = presetToken;
+    }
+  }
+
+  /**
+   * @typedef {Object} LoginOptions
+   * @property {string} username
+   * @property {string} password
+   */
+  /**
+   * @param {LoginOptions}
+   * @returns {Promise<User>}
+   */
+  async login({ username, password }) {
+    const resp = await fetch(`${this._serverBaseUrl}/api/v1/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const body = await resp.json();
+    if (resp.ok) {
+      this._token = body.token;
+      localStorage.setItem(this._tokenKey, body.token);
+      return jwt.decode(body.token);
+    } else {
+      throw new Error(body.message);
+    }
+  }
+
+  /**
+   * @typedef {Object} SignupOptions
+   * @property {string} username
+   * @property {string} password
+   * @property {string} firstname
+   * @property {string} lastname
+   */
+  /**
+   * @param {SignupOptions}
+   * @returns {Promise<User>}
+   */
+  async signup({ username, password, firstname, lastname }) {
+    const resp = await fetch(`${this._serverBaseUrl}/api/v1/auth/signup`, {
+      method: "POST",
+      body: JSON.stringify({ username, password, firstname, lastname }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const body = await resp.json();
+    if (resp.ok) {
+      this._token = body.token;
+      localStorage.setItem(this._tokenKey, body.token);
+      return jwt.decode(body.token);
+    } else {
+      throw new Error(body.message);
+    }
+  }
+}
