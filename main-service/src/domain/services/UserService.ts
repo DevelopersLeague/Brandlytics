a490851f4d774976d409981c1185967d01f5210c
aniket <aniketavinashmore343@gmail.com>
2021-09-23 11:58:04 +0530

    add userservice and test

--- main-service/src/domain/services/UserService.ts
+++ main-service/src/domain/services/UserService.ts
@@ -1,14 +1,63 @@
-import { IUserRepository, IUserService } from '../interfaces';
+import {
+  IUser,
+  IUserCreateDTO,
+  IUserRepository,
+  IUserService,
+  IUserUpdateDTO,
+} from '../interfaces';
 import { injectable, singleton, inject } from 'tsyringe';
+import { CustomPromisifyLegacy } from 'util';
+import { APIError } from '../utils';
 
 @injectable()
 @singleton()
-export class UserService implements Partial<IUserService> {
+export class UserService implements IUserService {
   constructor(
     @inject('user_repository')
-    userRepo: IUserRepository
-  ) 
-  {}
+    private readonly userRepo: IUserRepository
+  ) {}
+
+  public async createUser(dto: IUserCreateDTO): Promise<IUser> {
+    console.log(this.userRepo);
+    const existingUser = await this.userRepo.findOneByUsername(dto.username);
+    if (existingUser) {
+      throw APIError.invalidRequest('username already taken');
+    }
+    const user = this.userRepo.create(dto);
+    return user;
+  }
+
+  public async deleteUser(id: number): Promise<IUser> {
+    const user = await this.userRepo.delete(id);
+    if (!user) {
+      throw APIError.notFound('user not found');
+    }
+    return user;
+  }
+
+  public async updateUser(id: number, dto: IUserUpdateDTO): Promise<IUser> {
+    return {} as IUser;
+  }
+
+  public async getUserById(id: number): Promise<IUser> {
+    const user = await this.userRepo.findOneById(id);
+    if (!user) {
+      throw APIError.notFound('user not found');
+    }
+    return user;
+  }
+
+  public async getUserByUsername(username: string): Promise<IUser> {
+    const user = await this.userRepo.findOneByUsername(username);
+    if (!user) {
+      throw APIError.notFound('user not found');
+    }
+    return user;
+  }
+
+  public async getAllUsers(): Promise<IUser[]> {
+    return [] as IUser[];
+  }
 
   /*
   createUser: (userCreateDto: IUserCreateDTO) => Promise<IUser>;
---
+++ main-service/src/domain/services/tests/UserService.spec.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata';
+import { UserService } from '../index';
+import { IUser, IUserRepository, IUserService } from '../../interfaces';
+
+const makeUser = ({
+  id,
+  firstname,
+  lastname,
+  username,
+  password,
+  isDeleted,
+}: any): IUser => {
+  if (!id) {
+    id = 100;
+  }
+  if (!firstname) {
+    firstname = 'aniket';
+  }
+  if (!lastname) {
+    lastname = 'more';
+  }
+  if (!username) {
+    username = 'aniket_more';
+  }
+  if (!password) {
+    password = 'password';
+  }
+  if (!isDeleted) {
+    isDeleted = false;
+  }
+  return {
+    id,
+    firstname,
+    lastname,
+    username,
+    password,
+    isDeleted,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+};
+
+// describe('UserService', () => {});
