d7b953e10d37677c58e0c8565a5eeaa78651d7dd
aniket <aniketavinashmore343@gmail.com>
2021-09-23 11:57:39 +0530

    update interface

--- main-service/src/domain/interfaces/services.ts
+++ main-service/src/domain/interfaces/services.ts
@@ -2,7 +2,7 @@ import { IUserCreateDTO, IUser, IUserUpdateDTO } from './index';
 
 export interface IUserService {
   createUser: (userCreateDto: IUserCreateDTO) => Promise<IUser>;
-  updateUser: (userUpdateDto: IUserUpdateDTO) => Promise<IUser>;
+  updateUser: (id: number,userUpdateDto: IUserUpdateDTO) => Promise<IUser>;
   deleteUser: (id: number) => Promise<IUser>;
   getUserById: (id: number) => Promise<IUser>;
   getUserByUsername: (username: string) => Promise<IUser>;
