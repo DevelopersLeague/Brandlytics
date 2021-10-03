bb9b573c57f695aff0d4aaa0d281b248fd6f585e
aniket <aniketavinashmore343@gmail.com>
2021-10-03 18:46:36 +0530

    complete implementation

--- main-service/src/domain/services/SentimentService.ts
+++ main-service/src/domain/services/SentimentService.ts
@@ -1,6 +1,8 @@
 import { injectable, inject, singleton } from 'tsyringe'
-import { IAnalysisService, ISentimentService, ITweet, ITwitterAPIService, SentimentReport } from '../interfaces';
+import { IAnalysedTweet, IAnalysisService, ISentimentService, ITwitterAPIService, SentimentItem, SentimentReport } from '../interfaces';
 
+@injectable()
+@singleton()
 export class SentimentService implements ISentimentService {
   constructor(
     @inject('twitter_api_service')
@@ -9,71 +11,57 @@ export class SentimentService implements ISentimentService {
     private readonly analysisService: IAnalysisService
   ) { }
 
-  public async getSentiment(term: string, opts?: {
-    until: string
-  }): Promise<SentimentReport> {
-    let until;
-    if (opts && opts.until) {
-      until = opts.until;
-    }
-    else {
-      until = new Date().toISOString().split('T')[0]
-    }
-    let tweets: ITweet[] = [];
-    const iterations = 2;
-    for (let i = 0; i < iterations; i++) {
-      const resp = await this.apiService.searchTweets(term, {
-        until: until,
-        count: 100,
+  public async getSentiment(term: string): Promise<SentimentReport> {
+    const oneWeekBack = new Date(Date.now() - 6 * 24 * 60 * 60 * 1000);
+    let date = new Date(oneWeekBack.getTime() + 1 * 24 * 60 * 60 * 1000);
+    const tweets: IAnalysedTweet[] = []
+    const tweetsMap: { [key: string]: IAnalysedTweet[] } = {}
+    for (let i = 0; i < 7; i++) {
+      // date is one day ahead of expected date for tweets because of the behaviour of the api
+      // fetch tweets for each date
+      const respTweets = await this.apiService.searchTweets(term, {
+        until: date.toISOString().split('T')[0],
+        count: 100
       })
-      tweets = [...tweets, ...resp]
+      // analyse them
+      const analysed = await this.analysisService.analyseITweets(respTweets);
+      // collect them
+      tweetsMap[(new Date(date.getTime() - 1 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0]] = analysed
+      date = new Date(date.getTime() + 1 * 24 * 60 * 60 * 1000);
     }
-    const analysedTweets = await this.analysisService.analyseITweets(tweets);
-    const totalTweets = iterations * 100;
-    /*
-    let posCount = 0;
-    let negCount = 0;
-    let neuCount = 0;
-    let posExampleLeft = 5;
-    let negExampleLeft = 5;
-    let neuExampleLeft = 5;
-    // eslint-disable-next-line
-    let posExamples: ITweet[] = []
-    // eslint-disable-next-line
-    let negExamples: ITweet[] = []
-    // eslint-disable-next-line
-    let neuExamples: ITweet[] = []
-
-    analysedTweets.forEach(tweet => {
-      if (tweet.sentiment === 'positive') {
-        posCount = posCount + 1;
-        if (posExampleLeft > 0) {
-          posExampleLeft = posExampleLeft - 1;
-          posExamples.push(tweet)
-        }
+    const report: SentimentReport = {
+      sentiments: [],
+      examples: {
+        "positive": [],
+        "negative": [],
       }
-      if (tweet.sentiment === 'negative') {
-        negCount = negCount + 1;
-        if (negExampleLeft > 0) {
-          negExampleLeft = negExampleLeft - 1;
-          negExamples.push(tweet)
-        }
+    }
+    const examplesSize = 5;
+    // sort them in right date if not ordered
+    Object.keys(tweetsMap).forEach((key) => {
+      const reportItem: SentimentItem = {
+        date: key,
+        positive: 0,
+        negative: 0,
+        total: 0
       }
-      if (tweet.sentiment === 'neutral') {
-        neuCount = neuCount + 1;
-        if (neuExampleLeft > 0) {
-          neuExampleLeft = neuExampleLeft - 1;
-          neuExamples.push(tweet)
+      tweetsMap[key].forEach((tweet, index) => {
+        if (tweet.sentiment === "positive") {
+          reportItem.positive += 1;
+          if (report.examples['positive'].length < examplesSize) {
+            report.examples['positive'].push(tweet);
+          }
         }
-      }
+        if (tweet.sentiment === "negative") {
+          reportItem.negative += 1;
+          if (report.examples['negative'].length < examplesSize) {
+            report.examples['negative'].push(tweet);
+          }
+        }
+        reportItem.total += 1;
+      })
+      report.sentiments.push(reportItem)
     })
-
-    const sentimentReport: SentimentReport = {
-      sentiments: {
-        "positive": (posCount * 100) / totalTweets
-      }
-    }
-  */
-    return {} as SentimentReport
+    return report;
   }
 }
