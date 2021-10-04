abcb66297272a244c0a76581163d66eb9788cde9
aniket <aniketavinashmore343@gmail.com>
2021-10-04 10:38:04 +0530

    add sentiment controller

---
+++ main-service/src/web/controllers/SentimentController.ts
@@ -0,0 +1,36 @@
+import { Router, Request, Response } from 'express';
+import { singleton, injectable, inject } from 'tsyringe'
+import * as yup from 'yup'
+import { ISentimentService } from '../../domain/interfaces';
+import { IBaseController } from '../app';
+import { validate } from '../middleware';
+
+@singleton()
+@injectable()
+export class SentimentController implements IBaseController {
+  public path = "/api/v1/sentiment";
+  constructor(
+    @inject('sentiment_service')
+    private readonly sentimentService: ISentimentService
+  ) {
+
+  }
+  public getRouter(): Router {
+    const router = Router()
+    router.get('/week', validate({
+      query: yup.object().shape({
+        term: yup.string().required()
+      })
+    }), this.getSentiment.bind(this))
+    return router;
+  }
+
+  public async getSentiment(req: Request, res: Response): Promise<any> {
+    let term = ""
+    if (req.query.term) {
+      term = req.query.term as string
+    }
+    const resp = await this.sentimentService.getSentiment(term)
+    res.json(resp)
+  }
+}
