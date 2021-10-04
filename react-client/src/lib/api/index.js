1fff52df6647280c2854f102d64bdc7c35d35db9
aniket <aniketavinashmore343@gmail.com>
2021-10-04 10:58:34 +0530

    add sentiment support in api client

--- react-client/src/lib/api/index.js
+++ react-client/src/lib/api/index.js
@@ -84,4 +84,45 @@ export class APIClient {
       throw new Error(body.message);
     }
   }
+
+  /**
+   * @typedef {Object} Tweet
+   * @property {string} id
+   * @property {string} text
+   * @property {string} createdAt
+   * @property {string} username
+   * @property {string} sentiment
+   */
+
+  /**
+   * @typedef {Object} SentimentItem
+   * @property {string} date
+   * @property {number} positive
+   * @property {number} negative
+   * @property {number} total
+   */
+  /**
+   * @typedef {Object} SentimentReport
+   * @property {Array<SentimentItem>} sentiments
+   * @property {Object<string,Tweet[]>} examples
+   */
+  /**
+   * @param {string} term search term
+   * @returns {Promise<SentimentReport>}
+   */
+  async getSentiment(term) {
+    term = encodeURIComponent(term);
+    const params = new URLSearchParams(["term", term]);
+    const resp = await fetch(
+      `${this._serverBaseUrl}/api/v1/sentiment/week?` + params.toString(),
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const respJson = await resp.json();
+    return respJson;
+  }
 }
