c33b6717d0bb9d6aadbebf9fe2871a3e0723eba0
aniket <aniketavinashmore343@gmail.com>
2021-10-07 21:40:31 +0530

    prepare for presentation

--- sentiment-service/src/analysis_service/main.py
+++ sentiment-service/src/analysis_service/main.py
@@ -1,12 +1,112 @@
-import random
+import pickle
+import re
+import nltk
+import pandas as pd
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+# import wordnet
+# nltk.download('wordnet')
+
+# Defining dictionary containing all emojis with their meanings.
+emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad',
+          ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+          ':-@': 'shocked', ':@': 'shocked', ':-$': 'confused', ':\\': 'annoyed',
+          ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+          '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink',
+          ';-)': 'wink', 'O:-)': 'angel', 'O*-)': 'angel', '(:-D': 'gossip', '=^.^=': 'cat'}
 
 
-def analyse(text: str) -> str:
-    number = random.random()
-    sentiment = ""
-    if number >= 0.5:
-        sentiment = "negative"
-    else:
-        sentiment = "positive"
+def preprocess(textdata):
+    processedText = []
 
-    return sentiment
+    # Create Lemmatizer and Stemmer.
+    wordLemm = WordNetLemmatizer()
+
+    # Defining regex patterns.
+    urlPattern = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
+    userPattern = '@[^\s]+'
+    alphaPattern = "[^a-zA-Z0-9]"
+    sequencePattern = r"(.)\1\1+"
+    seqReplacePattern = r"\1\1"
+
+    for tweet in textdata:
+        tweet = tweet.lower()
+
+        # Replace all URls with 'URL'
+        tweet = re.sub(urlPattern, ' URL', tweet)
+        # Replace all emojis.
+        for emoji in emojis.keys():
+            tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])
+        # Replace @USERNAME to 'USER'.
+        tweet = re.sub(userPattern, ' USER', tweet)
+        # Replace all non alphabets.
+        tweet = re.sub(alphaPattern, " ", tweet)
+        # Replace 3 or more consecutive letters by 2 letter.
+        tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
+        tweetwords = ''
+        for word in tweet.split():
+            # Checking if the word is a stopword.
+            # if word not in stopwordlist:
+            if len(word) > 1:
+                # Lemmatizing the word.
+                word = wordLemm.lemmatize(word)
+                tweetwords += (word+' ')
+
+        processedText.append(tweetwords)
+
+    return processedText
+
+
+def load_models():
+    '''
+    Replace '..path/' by the path of the saved models.
+    '''
+
+    # Load the vectoriser.
+    file = open('vectoriser-ngram-(1,2).pickle', 'rb')
+    vectoriser = pickle.load(file)
+    file.close()
+    # Load the LR Model.
+    file = open('Sentiment-LR.pickle', 'rb')
+    LRmodel = pickle.load(file)
+    file.close()
+
+    return vectoriser, LRmodel
+
+
+def predict(vectoriser, model, text):
+    # Predict the sentiment
+    textdata = vectoriser.transform(preprocess(text))
+    sentiment = model.predict(textdata)
+
+    # Make a list of text with sentiment.
+    data = []
+    for text, pred in zip(text, sentiment):
+        data.append((text, pred))
+
+    # Convert the list into a Pandas DataFrame.
+    df = pd.DataFrame(data, columns=['text', 'sentiment'])
+    df = df.replace([0, 1], ["negative", "positive"])
+    return df
+
+
+# if __name__ == "__main__":
+#     # Loading the models.
+#     vectoriser, LRmodel = load_models()
+
+#     # Text to classify should be in a list.
+#     # text = ["I hate twitter", "May the Force be with you."]
+
+#     text = ["I hate twitter", "I love twitter"]
+#     df = predict(vectoriser, LRmodel, text)
+#     print(type(df['sentiment'][0]))
+#     print(type("aniket"))
+#     # print(df['sentiment'].head())
+
+vectoriser, LRmodel = load_models()
+
+
+def analyse(text):
+    df = predict(vectoriser, LRmodel, [text])
+    return df['sentiment'][0]
