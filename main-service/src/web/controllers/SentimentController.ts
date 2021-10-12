9d74bfe4948dd70fec34a419df502829e7306fcd
aniket <aniketavinashmore343@gmail.com>
2021-10-12 17:25:32 +0530

    log resp

--- main-service/src/web/controllers/SentimentController.ts
+++ main-service/src/web/controllers/SentimentController.ts
@@ -31,6 +31,7 @@ export class SentimentController implements IBaseController {
       term = req.query.term as string
     }
     const resp = await this.sentimentService.getSentiment(term)
+    console.log(resp)
     res.json(resp)
   }
 }
