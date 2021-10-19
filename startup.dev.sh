e84bdeab81c703d1b76ac55880040d8b80106be4
aniket <aniketavinashmore343@gmail.com>
2021-10-19 20:05:50 +0530

    make changes to script

--- startup.dev.sh
+++ startup.dev.sh
@@ -1,2 +1,2 @@
 export USE_MOCK_ANALYSIS_SERVICE="true"
-/c/Users/anike/AppData/Roaming/npm/concurrently "cd react-client && npm start" "cd main-service && npm start"
+$HOME/AppData/Roaming/npm/concurrently "cd react-client && npm start" "cd main-service && npm start"
